1c76dcd7323f5ec4f9b8b535dad5fb5a42f2db05
Hector Felipee <rexnike12@gmail.comm>
2022-11-29 15:22:08 -0500

    segundo commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http; 
+
+
+//uri permite identificar el recurso e identificarlo
+//url funaciona como localisador del servidoor
+
+getDataPokemon() async{
+  Uri _uri = Uri.parse("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+  http.Response response = await http.get(_uri);
+  print(response.statusCode);
+  print(response.body);
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Data"),
+          onPressed: () {
+            getDataPokemon();
+          },
+        ),
+      ),
+    );
   }
 }
